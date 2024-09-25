c562e6181ae08af1eac7413d57d0b8bbcf7a17ed
A-chan <mtureit@gmail.com>
2024-09-25 23:13:28 +0900

    GPTのおかげ

--- front-app/next/src/app/typings/html-to-image.d.ts
+++ front-app/next/src/app/typings/html-to-image.d.ts
@@ -1,3 +1,3 @@
 declare module "html-to-image" {
-  export function toPng(node: HTMLElement, options?: any): Promise<string>;
+  export function toPng(node: HTMLElement, options?: Record<string, unknown>): Promise<string>;
 }
